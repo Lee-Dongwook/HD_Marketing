6e2d0092fbb9400e270a7bee23acf3e844b02277
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-10-29 19:38:30 +0900

    feat: main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import localFont from "next/font/local";
 import "../../global.css";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -7,67 +6,6 @@ import "swiper/css/pagination";
 import SiteNav from "@/components/SiteNav";
 import Footer from "@/components/Footer";
 import QueryProvider from "../shared/provider/QueryProvider";
-
-const montserrat = localFont({
-  src: [
-    {
-      path: "../../public/fonts/montserrat/Montserrat-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/montserrat/Montserrat-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/montserrat/Montserrat-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/montserrat/Montserrat-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/montserrat/Montserrat-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/montserrat/Montserrat-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  variable: "--font-montserrat",
-});
-const poppins = localFont({
-  src: [
-    {
-      path: "../../public/fonts/poppins/Poppins-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/poppins/Poppins-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/poppins/Poppins-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/poppins/Poppins-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-poppins",
-});
 
 export default function RootLayout({
   children,
@@ -84,7 +22,7 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <style>{`html { -webkit-text-size-adjust: 100%; }`}</style>
       </head>
-      <body className={`${montserrat.variable} ${poppins.variable}`}>
+      <body>
         <QueryProvider>
           <SiteNav />
           <main className="min-h-screen">{children}</main>
