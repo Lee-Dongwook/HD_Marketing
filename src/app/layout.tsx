8413bb09b552a92c85d9bbaa252dc53b9e56a007
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-12-17 12:03:21 +0900

    fix: contact banner mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,28 +37,28 @@ export default function RootLayout({
   const pathname = usePathname();
   const isMainPage = pathname === "/";
 
-  if (isMobile || isTablet) {
-    return (
-      <html lang="ko" className="light" suppressHydrationWarning>
-        <head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
-          />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-        </head>
-        <body>
-          <MaintenancePage />
-        </body>
-      </html>
-    );
-  }
+  // if (isMobile || isTablet) {
+  //   return (
+  //     <html lang="ko" className="light" suppressHydrationWarning>
+  //       <head>
+  //         <meta
+  //           name="viewport"
+  //           content="width=device-width, initial-scale=1, maximum-scale=1"
+  //         />
+  //         <meta name="format-detection" content="telephone=no" />
+  //         <meta name="mobile-web-app-capable" content="yes" />
+  //         <meta name="apple-mobile-web-app-capable" content="yes" />
+  //         <meta
+  //           name="apple-mobile-web-app-status-bar-style"
+  //           content="default"
+  //         />
+  //       </head>
+  //       <body>
+  //         <MaintenancePage />
+  //       </body>
+  //     </html>
+  //   );
+  // }
 
   return (
     <html lang="ko" className="light" suppressHydrationWarning>
